3402a1a8237fa52e9c3e373952c673f53a5146ba
Mitchel <msf12@hampshire.edu>
2015-04-21 17:32:17 +0000

    OSPA3

--- workspace/OSAssignment3/src/MemoryManager.java
+++ workspace/OSAssignment3/src/MemoryManager.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 
 public class MemoryManager {
 	
-	private final int MEMSIZE,POLICY;
+	private final int POLICY;
 	private SegmentedMM smem;
+	private PagedMM pmem;
 	
 	public MemoryManager(int bytes, int policy) 
 	{
 		// intialize memory with these many bytes.
-		MEMSIZE = bytes;
 		POLICY = policy;
 		if(policy == 0)
 			smem = new SegmentedMM(bytes);
+		else
+			pmem = new PagedMM(bytes);
 	}
 
 	public int allocate(int bytes, int pid, int text_size, int data_size, int heap_size)
@@ -33,23 +35,8 @@ public class MemoryManager {
 			return -1;
 		}
 		else
-			return pageAllocate(bytes, pid);
-		//allocate this many bytes to the process with this id 
-		//assume that each pid is unique to a process 
-		//if using the Segmentation allocator: size of each segment is: text_size,
-		//..data_size, and heap_size.
-		//Verify that text_size + data_size + heap_size = bytes
-		//If using the paging allocator, simply ignore the segment size variables 
-		//Return 1 if successful 
-		//Return -1 if unsuccessful 
-		//Print an error indicating whether there wasn't sufficient memory or whether you
-		//..ran into external fragmentation
+			return pmem.allocate(bytes, pid);
 	 
-	}
-	
-	private int pageAllocate(int bytes, int pid)
-	{
-		return -1;
 	}
 
 	public int deallocate(int pid)
@@ -58,7 +45,10 @@ public class MemoryManager {
 		{
 			return smem.deallocate(pid);
 		}
-		return -1;
+		else
+		{
+			return pmem.deallocate(pid);
+		}
 		//deallocate memory allocated to this process
 		// return 1 if successful, -1 otherwise with an error message 
 	}
--- workspace/OSAssignment3/src/PagedMM.java
+++ workspace/OSAssignment3/src/PagedMM.java
@@ -1,4 +1,82 @@
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class PagedMM {
+	
+	private final int MAXPAGES;
+	boolean[] physpages;
+	private int numpages,
+				allocfails;
+	List<Process> prcs;
+
+	private class Process {
+		int id;
+		int size;
+		int[][] pages; //[virtpagenum][0] = physpagenum; [1] = bytes used
+		
+		public Process(int id, int size, int[][] pages)
+		{
+			this.id = id;
+			this.size = size;
+			this.pages = pages;
+		}
+		
+	}
+	
+	public PagedMM(int memsize)
+	{
+		MAXPAGES = memsize/32;
+		numpages = 0;
+		prcs = new LinkedList<Process>();
+		allocfails = 0;
+		physpages = new boolean[MAXPAGES];
+	}
+	
+	public int allocate(int bytes, int pid)
+	{
+		int pagecount = (int) Math.ceil(bytes/32.0);
+		if(pagecount + numpages > MAXPAGES)
+		{
+			allocfails++;
+			return -1;
+		}
+		
+		int[][] pages = new int[pagecount][2];
+		for(int i = 0; i < pagecount; i++)
+		{
+			pages[i][0] = -1;
+		}
+		
+		for(int i = 0,j = 0; i < MAXPAGES && pages[pagecount-1][0] == -1; i++)
+		{
+			if(!physpages[i])
+			{
+				pages[j][0] = i;
+				pages[j][1] = 32;
+			}
+		}
+		pages[pagecount-1][1] = (bytes+31)%32 + 1;
+		
+		prcs.add(new Process(pid, bytes, pages));
+		
+		return 0;
+	}
+
+	public int deallocate(int pid)
+	{
+		for(int i = 0; i < prcs.size(); i++)
+		{
+			if(prcs.get(i).id == pid)
+			{
+				prcs.remove(i);
+				return 0;
+			}
+		}
+		System.out.println("Process with id " + pid + " not found");
+		return -1;
+		//deallocate memory allocated to this process
+		// return 1 if successful, -1 otherwise with an error message 
+	}
 
 }
